1357538b4c94f766b24fc7da0e68d7ee2a13d58c
Finn Llewellyn <llewellynfinn@gmail.com>
2022-05-15 19:28:05 +0100

    Added required attributes to input elements, added text body above form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,39 @@
 const Contact = () => {
 	return (
-		<form name="contact" method="POST" className="contact__container">
-			<input type="hidden" name="form-name" value="contact" />
-			<div className="contact__inputs">
-				<p className="hidden">
-					<label>
-						Don't fill this out if you're human:
-						<input name="bot-field" />
-					</label>
-				</p>
-				<input type="text" placeholder="Name" />
-				<input type="email" name="email" placeholder="Email" />
-			</div>
-			<textarea name="message" placeholder="Message"></textarea>
-			<input
-				type="submit"
-				value="Submit"
-				className="btn btn--primary contact__btn transition"
-			/>
-		</form>
+		<>
+			<p className="contact__text">
+				I'm actively looking for jobs at the moment, so please feel free
+				to get in touch if you fancy offering me one.
+			</p>
+			<form name="contact" method="POST" className="contact__container">
+				<input type="hidden" name="form-name" value="contact" />
+				<div className="contact__inputs">
+					<p className="hidden">
+						<label>
+							Don't fill this out if you're human:
+							<input name="bot-field" />
+						</label>
+					</p>
+					<input type="text" placeholder="Name" required />
+					<input
+						type="email"
+						name="email"
+						placeholder="Email"
+						required
+					/>
+				</div>
+				<textarea
+					name="message"
+					placeholder="Message"
+					required
+				></textarea>
+				<input
+					type="submit"
+					value="Submit"
+					className="btn btn--primary contact__btn transition"
+				/>
+			</form>
+		</>
 	);
 };
 
